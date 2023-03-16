42ed2cf105dcdc9d871acb0328deffc67ef6058b
igordeandrade1 <116108662+igordeandrade1@users.noreply.github.com>
2023-03-16 16:45:16 -0300

    Commit 16 03
    Troquei o button do ambos por um input, para linkar a página final que irá mostrar as peças.

---
+++ Front-end/final.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Peças</title>
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/horta" type="text/css" />
+</head>
+
+<body>
+  <header>
+    <a href="index.php"><button class="titulo">SóNosCompiuter</button></a>
+  </header>
+
+  <main>
+
+    <h1>Peças</h1>
+
+    <div class="final">
+  
+    <img  class="peca" src="img/peça.PNG" alt="btn-games">
+    <img  class="peca" src="img/peça.PNG" alt="btn-games">
+    <img  class="peca" src="img/peça.PNG" alt="btn-games">
+    
+
+    </div>
+    
+    </div>
+      
+  </main>
+</body>
+
+</html>
