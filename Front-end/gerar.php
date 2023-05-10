6dbc6edf1333eff10441fef9f6d09a37af56b43d
Bundinha Molhada <116108735+MamedsonPereira@users.noreply.github.com>
2023-05-10 17:58:47 -0300

    Atualização no gerar.php
    criação de variáveis para armazenar o maior valor da pontuação das GPUs e CPUs requeridas entre os softwares

--- Front-end/gerar.php
+++ Front-end/gerar.php
@@ -9,7 +9,10 @@
     $desempenho = isset($_POST['desempenho']) ? $_POST['desempenho'] : null;
     $software_nome_string = $_POST['txtsoftwares'];
     $array_softwares = explode(",", $software_nome_string); 
+    $pont_gpu_final = 0;
+    $pont_cpu_final = 0;
 
+    // Caso o desempenho Geral seja nulo ele cria as variaveis para o FPS e o Gráfico avançado
     if($desempenho == null) {
         $fps = $_POST['fps'];
         $grafico = $_POST['grafico'];
@@ -29,26 +32,43 @@
         }
     }
 
-    $query = "select pontuacao_gpu from requisito_software where nome = '{$software_nome}'";
-
-    $result_pont_gpu = mysqli_query($mysqli, $query);
-
-    while ($pontuacao_gpu = $result_pont_gpu->fetch_assoc()) {
-        echo "Pontuação da GPU: $pontuacao_gpu['pontuacao_gpu'].<br>";
-    }
-
-    $query = "select pontuacao_cpu from requisito_software where nome = '{$software_nome}'";
-
-    $result_pont_cpu = mysqli_query($mysqli, $query);
-
-    while ($pontuacao_cpu = $result_pont_cpu->fetch_assoc()) {
-        echo "Pontuação da CPU: $pontuacao_cpu['pontuacao_gpu'].<br>";
-    }
+    for($i = 0; $i < count($array_softwares); $i++){
+        // Pega as pontuações da GPU recomendada para rodar os softwares
+        $query = "select pontuacao_gpu from requisito_software where nome = '{$array_softwares[$i]}'";
     
-    echo $orcamento . "<br>";
-    echo $fps . "<br>";
-    echo $grafico . "<br>";
-    print_r($array_softwares);
-    echo $software_nome_string . "<br>";
-    echo count($array_softwares);
+        $result_pont_gpu = mysqli_query($mysqli, $query);
+
+        // Cria uma váriavel para armazenar a informação vinda do banco de dados
+        while ($resultado = $result_pont_gpu->fetch_assoc()) {
+            $pontuacao_gpu = $resultado['pontuacao_gpu'];
+        } 
+
+        if($pont_gpu_final < $pontuacao_gpu){
+            $pont_gpu_final = $pontuacao_gpu;
+        } 
+
+        // Pega as pontuações da CPU recomendada para rodar os softwares
+        $query = "select pontuacao_cpu from requisito_software where nome = '{$array_softwares[$i]}'";
+
+        $result_pont_cpu = mysqli_query($mysqli, $query);
+
+        // Cria uma váriavel para armazenar a informação vinda do banco de dados
+        while ($resultado = $result_pont_cpu->fetch_assoc()) {
+            $pontuacao_cpu = $resultado['pontuacao_cpu'];
+        }
+
+        if($pont_cpu_final < $pontuacao_cpu){
+            $pont_cpu_final = $pontuacao_cpu;
+        } 
+    }
+
+    echo "Pontuaçao GPU: $pont_gpu_final <br>";
+    echo "Pontuaçao CPU: $pont_cpu_final <br>";
+    echo "Orçamento: $orcamento <br>";
+    echo "Fps: $fps <br>";
+    echo "grafico $grafico <br>";
+
+    for($i = 0; $i < count($array_softwares); $i++){
+        echo "Jogo: $array_softwares[$i] <br>";
+    }
 ?>
