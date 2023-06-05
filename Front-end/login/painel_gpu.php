2bc80bca80d8d792f9424cc7e4ee720a3452748d
igordeandrade1 <116108662+igordeandrade1@users.noreply.github.com>
2023-06-05 19:28:27 -0300

    Painel GPU
    Estilizei a o painel gpu, utilizando containers flexbox.

--- Front-end/login/painel_gpu.php
+++ Front-end/login/painel_gpu.php
@@ -28,16 +28,16 @@
 
     <h1>Painel GPU</h1>
     <form action="" method="POST">
-    <div class="games">
+    <div class="painel">
 
         <table border = "1">
             <tr>
-                <td>ID</td>
-                <td>Nome</td>
-                <td>Marca</td>
-                <td>Chip Gráfico</td>
-                <td>Pontuação</td>
-                <td>Preço</td>
+                <td class="tabela">ID</td>
+                <td class="tabela">Nome</td>
+                <td class="tabela">Marca</td>
+                <td class="tabela">Chip Gráfico</td>
+                <td class="tabela">Pontuação</td>
+                <td class="tabela">Preço</td>
             </tr>
             <?php
 
@@ -47,27 +47,28 @@
                 // Exibir os registros
                 while ($registro = $consulta->fetch(PDO::FETCH_ASSOC)) {
                     echo "<tr>";
-                    echo '<td >' . $registro['id'] . '</td>';
-                    echo '<td >' . $registro['nome_gpu'] . '</td>';
-                    echo '<td >' . $registro['marca'] . '</td>';
-                    echo '<td >' . $registro['chip_grafico'] . '</td>';
-                    echo '<td >' . $registro['pontuacao'] . '</td>';
-                    echo '<td >' . $registro['preco'] . '</td>';
-                    echo '<td> <a href="?delete='.$registro['id'].'">(X)</a> </td>';
-                    echo '<td> <a href="edit_gpu.php?id='.$registro['id'].'">(E)</a> </td>';
+                    echo '<td class="id-peca">' . $registro['id'] . '</td>';
+                    echo '<td class="id-peca">' . $registro['nome_gpu'] . '</td>';
+                    echo '<td class="id-peca">' . $registro['marca'] . '</td>';
+                    echo '<td class="id-peca">' . $registro['chip_grafico'] . '</td>';
+                    echo '<td class="id-peca">' . $registro['pontuacao'] . '</td>';
+                    echo '<td class="id-peca">' . $registro['preco'] . '</td>';
+                    echo '<td> <a  class="x" href="?delete='.$registro['id'].'">(X)</a> </td>';
+                    echo '<td> <a  class="e" href="edit_gpu.php?id='.$registro['id'].'">(E)</a> </td>';
                     echo "</tr>";
                 }
             ?>
         </table>
+        
+        <div class="painel-container">
+            <div class="div-painel">
+                <p class="style-p"><a class="style-href" href="cadastro_gpu.php">Cadastrar GPU</p>
+            </div>
 
-        <div class="div-painel">
-            <p class="style-p"><a class="style-href" href="cadastro_gpu.php">Cadastrar GPU</p>
-        </div>
-
-        <div class="div-painel">
-            <p class="style-p"><a class="style-href" href="painel.php">Voltar</p>
-        </div>
-
+            <div class="div-painel">
+                <p class="style-p"><a class="style-href" href="painel.php">Voltar</p>
+            </div>
+        </div>    
     </div>
     </form>
 </body>
